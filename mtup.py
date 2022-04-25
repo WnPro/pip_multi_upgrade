354b3d9ae02a99aa2fbfa769085ee2a6d7d8d946
wuniu <wuniu@swing>
2022-04-25 22:51:39 +0800

    Change code style

--- mtup.py
+++ mtup.py
@@ -12,14 +12,14 @@ from re import split
 from multiprocessing import Pool, Manager
 
 PIPCHECK = ['pip', 'list', '--user', '-o']
-__FENC = 'gbk' if (sys.platform.startswith('win32')) else 'utf-8'
-__PIPCOMMAND = ["pip", "install", "-U"]
-__PYEXEC = [sys.executable, '-m']
+_FENC = 'gbk' if (sys.platform.startswith('win32')) else 'utf-8'
+_PIPCOMMAND = ["pip", "install", "-U"]
+_PYEXEC = [sys.executable, '-m']
 
 
-def __write_log(name, log):
+def _write_log(name, log):
     ''' 向Errorlog.txt 写入日志 '''
-    with open("Errorlog.txt", mode="a", encoding=__FENC) as file:
+    with open("Errorlog.txt", mode="a", encoding=_FENC) as file:
         file.write(time.asctime() + f" {name}:\n")
         file.write(log + '\n')
 
@@ -28,33 +28,33 @@ def __write_log(name, log):
     print(f"Error log has been written in {log_path}")
 
 
-def __pip_upgrade(lock, name):
+def _pip_upgrade(lock, name):
     '''
     升级包，并在发生错误时写入错误日至
     写入错误日至时阻塞程序运行
     '''
-    cmd = __PYEXEC + __PIPCOMMAND
+    cmd = _PYEXEC + _PIPCOMMAND
     cmd.append(name)
     try:
-        run(cmd, encoding=__FENC, capture_output=True, check=True)
+        run(cmd, encoding=_FENC, capture_output=True, check=True)
         print(f"Package {name} has been upgraded.")
         return 0
     except CalledProcessError as error:
         print(f"An Error raised when {name} was upgrading.")
         lock.acquire()
-        __write_log(f"Error when upgrade {name}", error.stderr)
+        _write_log(f"Error when upgrade {name}", error.stderr)
         lock.release()
         return 1
 
 
-def __check_outdate():
+def _check_outdate():
     '''
     检查过时的包，返回一个列表
     '''
-    cmd = __PYEXEC + PIPCHECK
+    cmd = _PYEXEC + PIPCHECK
     outdate = run(cmd, check=True,
-            capture_output=True, encoding=__FENC)
-    __write_log("Check for upgrade", outdate.stderr)
+            capture_output=True, encoding=_FENC)
+    _write_log("Check for upgrade", outdate.stderr)
     lst = split(r"\s+", outdate.stdout)
     return lst[8:-2:4]
 
@@ -62,7 +62,7 @@ def __check_outdate():
 def mtupgrade():
     ''' 唤起进程池升级包 '''
     error_lock = Manager().RLock()
-    pkgnames = __check_outdate()
+    pkgnames = _check_outdate()
     ret_l = []
     errors = 0
     if len(pkgnames) != 0:
@@ -73,7 +73,7 @@ def mtupgrade():
             return errors
         upgrade_processing = Pool()
         for j in pkgnames:
-            ret = upgrade_processing.apply_async(__pip_upgrade,
+            ret = upgrade_processing.apply_async(_pip_upgrade,
                     args=(error_lock, j))
             ret_l.append(ret)
         upgrade_processing.close()
